8c384ffa14edd0676705623b767ff3c8f856b639
viswanadhulasaideepak <151038199+viswanadhulasaideepak@users.noreply.github.com>
2026-02-03 20:54:52 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,144 @@
+//1.Welcome Program
+//-Ask user name using prompt().
+//A)
+//prompt("What is your name?")
+//-Show alert: Hi, Welcome to JavaScript Training.
+//A)
+//alert("Hi , Welcome to JavaScript Training")
+//Print the same message in console.
+//let a1 = alert("Hi , Welcome to JavaScript Training")
+//console.log(a1);
+//let a2= prompt("What is your name?")
+//console.log(a2)
+
+//2.Console Methods Practice
+//-Print 500 using console.log(), console.warn(), console.error()
+//A)
+//console.log(500)
+//console.warn(500)
+//console.error(500)
+//-Use console.clear()
+//console.clear()
+
+//3.Data Type Identification
+//-Create string, number, boolean, undefined, and null variables.
+//-Print value and type using typeof. 
+//A)
+//let a= "JavaScript"
+//console.log(a)
+//console.log(typeof(a))
+//let b= 250
+//console.log(b)
+//console.log(typeof(b))
+//let c
+//console.log(c)
+//console.log(typeof(c))
+//let d= prompt()
+//console.log(d)
+//console.log(typeof(d))
+
+//4.Arithmetic Operations
+//-Use let a= 20 and let b= 5.
+//-Perform+, -, *, %, ** and print results.
+//A)
+//a= 20
+//b= 5
+//console.log(a+b)
+//console.log(a-b)
+//console.log(a*b)
+//console.log(a%b)
+//console.log(a**b)
+
+//5.Increment & Decrement
+//-Demonstrate pre-increment, post-increment, pre-decrement, post-decrement.
+//-Print variables clearly.
+//A)
+//Pre-increment
+/*let a= 10
+b= ++a
+console.log("a:-",a)
+console.log("b:-",b)*/
+//Post-increment
+/*let c=25
+d= c++
+console.log("c:-",c)
+console.log("d:-", d)*/
+//Pre-decrement
+/*let e= 36
+f= --e
+console.log("e:-",e)
+console.log("f:-",f)*/
+//Post-decrement
+/*let g= 51
+h= g--
+console.log("g:-", g)
+console.log("h:-",h)*/
+
+//6.Assignment Operators
+//- Use let num= 10.
+//-perform +=, -=, /=, %= and print results.
+//A)
+/*let num= 10
+num = num + num
+console.log(num)*/
+/*let num= 10
+num= num - num
+console.log(num)*/
+/*let num= 10
+num= num* num
+console.log(num)*/
+/*let num= 10
+num= num/num
+console.log(num)*/
+/*let num= 10
+num= num % num
+console.log(num)*/
+
+//7. Array Access
+//- Create array['HTML','CSS', 'JavaScript', 'React'].
+//Print first element, second element, last element(using length), and total elements.
+//A)
+/*let a=['HTML', 'CSS', 'JavaScript', 'React']
+console.log(a[0])  
+console.log(a[1]) 
+console.log(a[a.length-1])
+console.log(a.length)*/
+
+//8.Modify Array
+//-Add one element at end.
+//-Print updated array
+//A)
+/*let a=['HTML', 'CSS', 'JavaScript', 'React']
+a.push('Django')
+console.log(a)*/
+/*let a=['HTML', 'CSS', 'JavaScript', 'Django' ,'React' , 'Flask']
+a.pop('Flask')
+console.log(a)*/
+
+//9.Student Object
+//-Create object with name, age, course, city.
+//-Print values using dot notation
+//A)
+/*let b={ name:'Sai', age: 22, course:'Associate Software Engineering', city: 'Hyderabad' }
+console.log(b.name)
+console.log(b.age)
+console.log(b.course)
+console.log(b.city)*/
+
+//10.Nested object Practice
+//- Create company object with nested trainer object.
+//-print company name, trainer name, trainer subject.
+//A)
+
+let company={ companyname:'Stackly', trainername:'Naveen', trainersubject:'JavaScript'}
+console.log(company.companyname)
+console.log(company.trainername)
+console.log(company.trainersubject)
+
+
+
+
+
+
+
+
